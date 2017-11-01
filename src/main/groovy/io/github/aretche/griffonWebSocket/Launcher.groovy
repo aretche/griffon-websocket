28b2308c3d03516c2f82e0e4da6845d15c8ed9fb
Gabriel Arellano <arellanog@cent.gov.ar>
2017-11-01 13:33:58 -0300

    Subo el nivel de log de la aplicación

--- src/main/groovy/io/github/aretche/griffonWebSocket/Launcher.groovy
+++ src/main/groovy/io/github/aretche/griffonWebSocket/Launcher.groovy
@@ -4,6 +4,8 @@ import griffon.javafx.JavaFXGriffonApplication
 
 public class Launcher {
     public static void main(String[] args) throws Exception {
+        System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "INFO")
+        System.setProperty("griffon.full.stacktrace", "true")
         JavaFXGriffonApplication.main(args)
     }
 }
